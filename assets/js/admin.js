2b5788df0de164a83103696f96f1a674a4a1f94d
Achim Galeski <achim.galeski@gmail.com>
2022-04-27 12:31:33 +0200

    added notification style & function

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,12 +1,46 @@
-(function($) {
+(function ($) {
 
-  'use strict';
+    'use strict';
 
-  $(".ag-admin-form").submit(function(event) {
-    event.preventDefault();
-    $.post(ajaxurl, $(event.target).serializeArray(), function(response) {
-      console.log(response);
+    let notice  = $('.ag-notification'),
+        header  = notice.find('h3'),
+        message = notice.find('p')
+    ;
+
+    function prepare(color) {
+        notice.hide()
+          .css('background-color', color)
+        ;
+    }
+
+    function flash () {
+        notice.show()
+            .delay(5000)
+            .fadeOut(1000, function () {
+                notice.hide();
+            })
+        ;
+    }
+
+    $(".ag-settings form").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url:  ajaxurl,
+            data: $(event.target).serializeArray(),
+            success: function (result) {
+                prepare('green');
+                header.text('Success');
+                message.text(result.data.message);
+                flash();
+            },
+            error: function (request) {
+                prepare('red');
+                header.text('Error');
+                message.text(request.responseJSON.data.error);
+                flash();
+            }
+        });
     });
-  });
 
 })(jQuery);
