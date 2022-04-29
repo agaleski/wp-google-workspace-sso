eaa0f4351007bdaaac289d66356ce441f9287b04
Achim Galeski <achim.galeski@gmail.com>
2022-04-29 21:58:33 +0200

    applied strict psr-12 to Settings::class

--- src/Settings.php
+++ src/Settings.php
@@ -9,7 +9,6 @@ namespace AGaleski\WordPress\GoogleWorkspaceSso;
  */
 class Settings
 {
-
     public const  OPTION      = WPGWSSO_PREFIX . 'settings';
     private const CIPHER_ALGO = 'aes-256-cbc';
     private const HASH_ALGO   = 'sha3-512';
@@ -34,9 +33,9 @@ class Settings
      *
      * @return bool
      */
-    public static function isActive() : bool
+    public static function isActive(): bool
     {
-        return self::get()[ 'active' ];
+        return (bool) self::get()[ 'active' ];
     }
 
     /**
@@ -44,7 +43,7 @@ class Settings
      *
      * @return array
      */
-    public static function getAccess() : array
+    public static function getAccess(): array
     {
         return self::get()[ 'access' ];
     }
@@ -54,14 +53,12 @@ class Settings
      *
      * @return array
      */
-    private static function get() : array
+    private static function get(): array
     {
         if (empty(self::$settings[ 'loaded' ])) {
-
             $settings                   = get_network_option(get_current_network_id(), self::OPTION, []);
             self::$settings             = array_replace(self::$settings, $settings);
             self::$settings[ 'loaded' ] = true;
-
         }
 
         return self::$settings;
@@ -74,7 +71,7 @@ class Settings
      *
      * @return bool
      */
-    public static function set(array $settings = []) : bool
+    public static function set(array $settings = []): bool
     {
         self::$settings = array_replace(self::$settings, $settings);
 
@@ -88,7 +85,7 @@ class Settings
      *
      * @return string
      */
-    public static function decrypt(string $string = '') : string
+    public static function decrypt(string $string = ''): string
     {
         $string   = base64_decode($string);
         $hash     = substr($string, 0, 64);
@@ -96,11 +93,10 @@ class Settings
         $ivLength = openssl_cipher_iv_length(self::CIPHER_ALGO);
 
         if (! hash_equals($hash, hash_hmac(self::HASH_ALGO, $content, self::getHashKey(), true))) {
-
             return '';
         }
 
-        return '' . openssl_decrypt(
+        return (string) openssl_decrypt(
             substr($content, $ivLength),
             self::CIPHER_ALGO,
             self::getPassphrase(),
@@ -116,7 +112,7 @@ class Settings
      *
      * @return string
      */
-    public static function encrypt(string $string = '') : string
+    public static function encrypt(string $string = ''): string
     {
         $iv      = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::CIPHER_ALGO));
         $message = openssl_encrypt($string, self::CIPHER_ALGO, self::getPassphrase(), OPENSSL_RAW_DATA, $iv);
@@ -130,18 +126,16 @@ class Settings
      *
      * @return string
      */
-    private static function getPassphrase() : string
+    private static function getPassphrase(): string
     {
         if (empty(self::get()[ 'passphrase' ])) {
-
             $ivLength                       = openssl_cipher_iv_length(self::CIPHER_ALGO);
             self::$settings[ 'passphrase' ] = base64_encode(openssl_random_pseudo_bytes($ivLength));
 
             update_network_option(get_current_network_id(), self::OPTION, self::$settings);
-
         }
 
-        return base64_decode(self::$settings[ 'passphrase' ]);
+        return (string) base64_decode(self::$settings[ 'passphrase' ]);
     }
 
     /**
@@ -149,17 +143,14 @@ class Settings
      *
      * @return string
      */
-    private static function getHashKey() : string
+    private static function getHashKey(): string
     {
         if (empty(self::get()[ 'hashKey' ])) {
-
             self::$settings[ 'hashKey' ] = base64_encode(openssl_random_pseudo_bytes(64));
 
             update_network_option(get_current_network_id(), self::OPTION, self::$settings);
-
         }
 
-        return base64_decode(self::$settings[ 'hashKey' ]);
+        return (string) base64_decode(self::$settings[ 'hashKey' ]);
     }
-
 }
