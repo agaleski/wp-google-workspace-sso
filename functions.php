838ee83c31f706e6d8cdba327ff65ff9eff9df7b
Achim Galeski <achim.galeski@gmail.com>
2022-06-07 11:17:14 +0200

    added license header

--- functions.php
+++ functions.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * WordPress Google Workspace SSO - WordPress plugin for Google single sign-on admin login via OAuth.
+ * Copyright (C) 2022 Achim Galeski ( achim-galeski@gmail.com )
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA02110-1301USA
+ *
+ * @version 1.0.0
+ */
 
 use AGaleski\WordPress\GoogleWorkspaceSso\Admin;
 use AGaleski\WordPress\GoogleWorkspaceSso\Login;
